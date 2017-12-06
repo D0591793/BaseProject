2141e0389ece9a287e3d64ac76c73b9cdd6d1c95
asd0591804 <lestia521328@gmail.com>
2017-12-06 12:06:30 +0800

    test

---
+++ src/test/java/com/github/asd0591804/testTest.java
@@ -0,0 +1,13 @@
+package com.github.asd0591804;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class testTest {
+    @Test
+    public void student() throws Exception {
+        assertEquals(0,0);
+    }
+
+}
