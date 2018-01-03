70da52836fdd0095dd2e0243ab625f065f1b4056
ych861031 <danny861031>
2018-01-03 15:51:58 +0800

    註解 and setbackground

--- src/main/java/com/github/team/frame.java
+++ src/main/java/com/github/team/frame.java
@@ -6,46 +6,51 @@ import java.util.logging.Level;
 
 public class frame {
     public static void main(String[] args){
-        JFrame jframe = new JFrame();
 
+        /*set frame*/
+        JFrame jframe = new JFrame();
         jframe.setSize(375,640);
         jframe.setLocationRelativeTo(null);
 
+
+        /*get contentpane and set background*/
+        jframe.getContentPane().setBackground(Color.white);
         jframe.setLayout(null);
 
+
+        /*set Label*/
         JLabel label = new JLabel("時間管理");
         label.setBounds(125,100,200,50);
         label.setFont(new Font("", Font.BOLD, 32));
         jframe.add(label);
 
 
-
-        Button btn1 = new Button("test1");
-        btn1.setBounds(157,200,60,25);
+        /*set Button*/
+        Button btn1 = new Button("排入上班課行程");
+        btn1.setBounds(110,200,150,25);
         jframe.add(btn1);
 
-        Button btn2 = new Button("test2");
-        btn2.setBounds(157,250,60,25);
+        Button btn2 = new Button("自動規劃娛樂活動");
+        btn2.setBounds(110,250,150,25);
         jframe.add(btn2);
 
-        Button btn3 = new Button("test3");
-        btn3.setBounds(157,300,60,25);
+        Button btn3 = new Button("鬧鐘");
+        btn3.setBounds(110,300,150,25);
         jframe.add(btn3);
 
-        Button btn4 = new Button("test4");
-        btn4.setBounds(157,350,60,25);
+        Button btn4 = new Button("在線專家");
+        btn4.setBounds(110,350,150,25);
         jframe.add(btn4);
 
-        Button btn5 = new Button("test5");
-        btn5.setBounds(157,400,60,25);
+        Button btn5 = new Button("排入其他待辦事項");
+        btn5.setBounds(110,400,150,25);
         jframe.add(btn5);
 
 
 
+        /*!!!!!important!!!!*/
         jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         jframe.setVisible(true);
-
-
 
     }
 
