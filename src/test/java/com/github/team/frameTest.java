cabfd83e4d1d4330be369623b2f7ece13ab45e7b
ych861031 <danny861031@gmail.com>
2018-01-09 13:24:38 +0800

    test coverage2

--- src/test/java/com/github/team/frameTest.java
+++ src/test/java/com/github/team/frameTest.java
@@ -1,18 +1,13 @@
 package com.github.team;
 
-import com.github.ych861031.setSchedule;
 import org.junit.Test;
 
 import javax.swing.*;
 
-import java.awt.event.ActionEvent;
 
 import static org.junit.Assert.*;
 
 public class frameTest{
-
-
-
 
     JFrame jframe = new JFrame();
     JFrame jframe2 = new JFrame();
