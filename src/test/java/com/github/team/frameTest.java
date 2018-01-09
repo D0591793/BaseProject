be6f88987b9b9a9b475f15d34607d56771c1f191
ych861031 <danny861031@gmail.com>
2018-01-09 13:35:25 +0800

    frame test3

---
+++ src/test/java/com/github/team/frameTest.java
@@ -0,0 +1,20 @@
+package com.github.team;
+
+import org.junit.Test;
+
+import javax.swing.*;
+
+import static org.junit.Assert.*;
+
+public class frameTest {
+
+
+    JFrame jframe = new JFrame();
+    JFrame jframe2 = new JFrame();
+
+    frame a = new frame(jframe,jframe2);
+    @Test
+    public void covergeTest() {
+        assertEquals(1,a.covergeTest(1));
+    }
+}
